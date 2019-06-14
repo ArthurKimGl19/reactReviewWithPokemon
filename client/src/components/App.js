632df8516697acc7684be7ff742fe6165fb930d0
Hack Reactor Student <student@hackreactor.com>
2019-06-13 21:28:46 -0700

    Added pokemon to state

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -6,16 +6,28 @@ class App extends React.Component {
   constructor(props){
     super(props);
     this.state = {
-
+      pokemon: []
     }
   }
   render() {
     return (
       <div>
        <Search />
-       <PokemonList pokemon={this.props.pokemon}/>
+       <PokemonList pokemon={this.state.pokemon}/>
       </div>   
     )
+  }
+  //after render we do this componentDidMount
+  componentDidMount(){
+    // setState() {
+    //   this.state.pokemon = this.props.pokemon;
+    // }
+    
+    //when component mounts
+    //we run set State, which is a method of our class App
+    //set State takes in an object with a key that is the key in our state
+    //and the value we want to be inside the key inside the state
+    this.setState({pokemon: this.props.pokemon});    
   }
 }
 
