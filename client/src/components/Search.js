bc407606b1b424240746df66505a11d44a04a413
AK <ak@AKs-MacBook-Pro.local>
2019-06-19 18:15:30 -0700

    Prior to moving Axios call and search methods to App

--- client/src/components/Search.js
+++ client/src/components/Search.js
@@ -1,4 +1,5 @@
 import React from 'react';
+const axios = require('axios');
 
 class Search extends React.Component {
   constructor(props){
@@ -12,20 +13,34 @@ class Search extends React.Component {
   }
   onSearchChange(event) {
     //on change it will update the state
+    var newValue = event.target.value.toLowerCase();
     this.setState({
-      value: event.target.value
+      value: newValue
     })
     console.log('Current Value ' + event.target.value)
   }
   onHandleSubmit(event){
+    event.preventDefault();
     //on pressing submit it will print out what I have in state
-    console.log(JSON.stringify('Submitting Value' + this.state.value))
+    console.log(JSON.stringify('Submitting Value ' + this.state.value))
+    //want to send an axios get request 
+    //our get req is going to go to exact pokemon link of what we submit
+    axios.get('https://pokeapi.co/api/v2/pokemon/' + this.state.value)
+      //based on response
+      .then(function(response){
+        //based on response, we want to update our state and render said pokemon we searched
+        console.log(response);
+      })
+      //handle error
+      .catch(function(error){
+        console.log(error);
+      });
   }
   render () {
     return (
       <div>
       <form action="" >
-        <input type="text" onChange={this.onSearchChange} value={this.state.value}/>
+        <input type="text" onChange={this.onSearchChange} value={this.state.value.toLowerCase()}/>
         <button onClick={this.onHandleSubmit}>Submit</button>
       </form>
       </div>
